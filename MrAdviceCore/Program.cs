b4c4500b8fd75b64d5e1a687dafe8b71469583a2
Alexander Buts <alexandr.buts@gmail.com>
2019-07-21 16:31:56 +0300

    Initial commit.

---
+++ MrAdviceCore/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using ArxOne.MrAdvice.Advice;
+
+namespace MrAdviceCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new TestClass().TestMethod();
+            Console.ReadLine();
+        }
+    }
+
+    public class TestClass
+    {
+        [Log]
+        public void TestMethod()
+        {
+            Console.WriteLine("This is a call.");
+        }
+    }
+
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+	public class LogAttribute : Attribute, IMethodAdvice
+	{
+		public void Advise(MethodAdviceContext context)
+		{
+            Console.WriteLine("Before call.");
+            context.Proceed();
+            Console.WriteLine("After call.");
+		}
+	}
+}
